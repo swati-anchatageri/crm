98250b737128184c7f2d6975aeff4a2f16095196
Swati Anchatageri <103919084+swati-anchatageri@users.noreply.github.com>
2024-12-17 11:49:20 +0530

    Delete OneDrive/Desktop/FinalAssests (3)/FinalAssests directory

--- OneDrive/Desktop/FinalAssests (3)/FinalAssests/formAPI/Client-Form/src/main/java/com/assetsglobal/clientform/Client/Form/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assetsglobal.clientform.Client.Form.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.assetsglobal.clientform.Client.Form.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
--- OneDrive/Desktop/FinalAssests (3)/FinalAssests/formAPI/Client-Form/src/main/java/com/assetsglobal/clientform/Client/Form/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.assetsglobal.clientform.Client.Form.service;
-
-import com.assetsglobal.clientform.Client.Form.entity.User;
-
-public interface UserService {
-
-	User addUser(User user);
-
-}
