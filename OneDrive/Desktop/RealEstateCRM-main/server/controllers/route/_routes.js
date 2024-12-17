1dac7fede8b408394fc69f91f8743e13f95661c4
Swati Anchatageri <103919084+swati-anchatageri@users.noreply.github.com>
2024-12-17 11:49:03 +0530

    Delete OneDrive/Desktop/RealEstateCRM-main directory

--- OneDrive/Desktop/RealEstateCRM-main/server/controllers/route/_routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const route = require('./route');
-const auth = require('../../middelwares/auth');
-
-const router = express.Router();
-
-router.get('/', auth, route.index)
-
-module.exports = router
--- OneDrive/Desktop/RealEstateCRM-main/server/controllers/status/_routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const auth = require('../../middelwares/auth');
-const status = require('./status');
-const router = express.Router();
-
-router.get('/', auth, status.index)
-
-module.exports = router
